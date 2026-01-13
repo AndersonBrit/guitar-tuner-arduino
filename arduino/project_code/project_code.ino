4af18bd3ee4d0f3c3bf608c8977e723f209e94fe
Anderson Brito <andersonbrito.dev@gmail.com>
2026-01-13 17:08:33 +0000

    Display Mostra valores - Piezo

--- arduino/project_code/project_code.ino
+++ arduino/project_code/project_code.ino
@@ -1,41 +1,54 @@
-#include <Wire.h>
 #include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
+#include <Adafruit_ST7789.h>
+#include <SPI.h>
 
-#define SCREEN_WIDTH 128
-#define SCREEN_HEIGHT 64
+// ==== Pinos do TFT (iguais ao teu código funcional) ====
+#define LCD_MOSI 23
+#define LCD_SCLK 18
+#define LCD_CS   15
+#define LCD_DC   2
+#define LCD_RST  4
+#define LCD_BLK  32
+
+// ==== Piezo ====
 #define PIEZO_PIN 34
 
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+Adafruit_ST7789 lcd = Adafruit_ST7789(LCD_CS, LCD_DC, LCD_RST);
 
 void setup() {
-  Wire.begin(21, 22); // pinos do TFT integrado
   Serial.begin(115200);
+  delay(500);
 
-  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
-    while(true); // trava aqui se TFT falhar
-  }
+  // Backlight
+  pinMode(LCD_BLK, OUTPUT);
+  digitalWrite(LCD_BLK, HIGH);
 
-  display.clearDisplay();
-  display.setTextSize(2);
-  display.setTextColor(WHITE);
-  display.setCursor(0,0);
-  display.println("Teste Piezo");
-  display.display();
+  // Inicializar TFT
+  lcd.init(135, 240);
+  lcd.setRotation(1);
+  lcd.fillScreen(ST77XX_BLACK);
+
+  // Texto inicial
+  lcd.setTextSize(2);
+  lcd.setTextColor(ST77XX_WHITE);
+  lcd.setCursor(0, 0);
+  lcd.println("Teste Piezo");
 }
 
 void loop() {
-  int valor = analogRead(PIEZO_PIN); // lê piezo
+  int valor = analogRead(PIEZO_PIN);
 
-  // Mostra valor no TFT
-  display.setCursor(0,30);
-  display.fillRect(0,30,128,34,BLACK); // limpa área anterior
-  display.print("Valor: ");
-  display.println(valor);
-  display.display();
+  // Limpa apenas a zona do valor
+  lcd.fillRect(0, 40, 240, 40, ST77XX_BLACK);
+  lcd.setCursor(0, 40);
+  lcd.setTextSize(2);
+  lcd.setTextColor(ST77XX_GREEN);
 
-  // Mostra também no Serial (opcional)
+  lcd.print("Valor: ");
+  lcd.println(valor);
+
+  // Serial (debug)
   Serial.println(valor);
 
-  delay(100); // atualiza a cada 100ms
+  delay(100);
 }
